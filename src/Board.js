b580fe661dc74c323bc0b690c9a7758a48af733c
robertng0407 <robertng0407@gmail.com>
2019-05-22 12:47:34 -0700

    Create board on start

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Cell from "./Cell";
-import './Board.css';
-
+import "./Board.css";
 
 /** Game board of Lights out.
  *
@@ -30,11 +29,18 @@ import './Board.css';
  **/
 
 class Board extends Component {
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25
+  };
 
   constructor(props) {
     super(props);
-
-    // TODO: set initial state
+    this.state = {
+      board: this.createBoard(),
+      hasWon: false
+    };
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -42,16 +48,23 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
-    return board
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn);
+      }
+      board.push(row);
+    }
+
+    return board;
   }
 
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
-    let {ncols, nrows} = this.props;
+    let { ncols, nrows } = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
-
 
     function flipCell(y, x) {
       // if this coord is actually on board, flip it
@@ -65,24 +78,29 @@ class Board extends Component {
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
-
-    this.setState({board, hasWon});
+    // this.setState({ board, hasWon });
   }
 
-
-  /** Render game board or winning message. */
-
   render() {
+    let tblboard = [];
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(<Cell isLit={this.state.board[y][x]} />);
+      }
+      tblboard.push(<tr>{row}</tr>);
+    }
 
+    return (
+      <table className="Board">
+        <tbody>{tblboard}</tbody>
+      </table>
+    );
     // if the game is won, just show a winning msg & render nothing else
-
     // TODO
-
     // make table board
-
     // TODO
   }
 }
-
 
 export default Board;
